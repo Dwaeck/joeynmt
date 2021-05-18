ee42c509fa3ec8f49e9ed975de37f2f355e37535
Dwaeck <83475968+Dwaeck@users.noreply.github.com>
2021-05-18 11:47:34 +0200

    Update training.py

--- joeynmt/training.py
+++ joeynmt/training.py
@@ -776,7 +776,8 @@ def train(cfg_file: str, skip_test: bool = False) -> None:
         data_cfg=cfg["data"])
 
     # build an encoder-decoder model
-    model = build_model(cfg["model"], src_vocab=src_vocab, trg_vocab=trg_vocab)
+    active_layers = cfg["testing"].get("alpha_layers",[])
+    model = build_model(cfg["model"],active_layers,src_vocab=src_vocab, trg_vocab=trg_vocab)
 
     # for training management, e.g. early stopping and model selection
     trainer = TrainManager(model=model, config=cfg)
